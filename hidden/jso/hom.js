9955974d2766137f275df482050405def8f75556
Roy <157291828+Rigriz@users.noreply.github.com>
2024-01-21 22:24:50 +0530

    Home.js uodate

---
+++ hidden/jso/hom.js
@@ -0,0 +1,55 @@
+//Full stackk Development
+function notes() {
+  var link = "note.html";
+  location.href = link;
+}
+function vid(){
+ var link ="./tutorial.html";
+location.href=link;
+}
+function qa(){
+ var link ="qa.html";
+ location.href = link;
+}
+//Learners Point
+var ht= "./links/ht.html";
+function html() {
+  location.href = ht;
+/*
+  document.getElementById("link1").addEventListener("click", function(){
+    document.getElementById("frame").src = "https://www.javatpoint.com/";
+  })
+  document.getElementById("link2").addEventListener('click', function(){
+    document.getElementById("frame").src="https://www.codewithharry.com/"; 
+  })
+  document.getElementById("link3").addEventListener('click', function(){
+  document.getElementById("frame").src="https://www.codewithharry.com/"; 
+  })*/
+  
+}
+function javasr() {
+  var java= "./links/javasr.html";
+  location.href = java;
+}
+function react() {
+  var react= "./links/react.html";
+  location.href = react;
+}
+
+function mongo() {
+  var mongo= "./links/mogo.html";
+  location.href = mongo;
+}
+
+function docker() {
+  var docker= "./links/docker.html";
+  location.href = docker;
+}
+function git() {
+  var git= "./links/git.html";
+  location.href = git;
+}
+function php() {
+  var php= "./links/php.html";
+  location.href = php;
+}
