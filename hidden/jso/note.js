71b39a0c233f6306dbc7e8782e1406c4acb9532a
Roy <157291828+Rigriz@users.noreply.github.com>
2024-01-26 14:24:49 +0530

    pdfviewer created

--- hidden/jso/note.js
+++ hidden/jso/note.js
@@ -1,24 +1,12 @@
 
 function data(note){
-    localStorage.setItem('Week', note);
+    localStorage.setItem('data', note);
     console.log("initiated")
 
 }
-function exam1(){
+function exam1(){}
    /*  window.location.assign(
         "https://1drv.ms/w/s!AvY3abs-bIvAgTep_PmsgRRoCfii?e=FTwhqd"
     );
     */
-   alert("Currently this page is not available");
-}
-function exam2(){
-    window.location.assign(
-        "https://1drv.ms/b/s!AvY3abs-bIvAgTuslvgQYCwFCJsu?e=g1prFF"
-    );
-}
-function exam3(){
-    window.location.assign(
-        "https://1drv.ms/b/s!AvY3abs-bIvAgTpNm0r7N2q7rfYm?e=SaHKsu"
-    );
-}
 
