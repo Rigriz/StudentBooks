9753f43bd3693abc4191da3dc3b4cd59d7ede229
Roy <157291828+Rigriz@users.noreply.github.com>
2024-01-26 21:29:29 +0530

    hmtl to js creation

---
+++ pd.js
@@ -0,0 +1,54 @@
+var aph = localStorage.getItem('data');
+console.log(aph)
+var aphint = parseInt(aph);
+console.log("entering obja")
+
+    if (aphint = 1) {
+        var url = "https://drive.google.com/file/d/1PsMvXECSNRTsZJKtrloaoTxIu1PwiTEg/view?usp=sharing"
+        console.log("week1 set");
+    }
+    if (aphint = 2) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 3) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 4) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 5) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 6) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 7) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 8) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 9) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 10) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 12) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }
+    if (aphint = 13) {
+        var url = "https://www.google.com"
+        console.log("week1 set");
+    }  
+    console.log(url);
