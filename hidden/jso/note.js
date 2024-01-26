7e48fa57f26224bf22ac701241bfc7bedb0097d1
Roy <157291828+Rigriz@users.noreply.github.com>
2024-01-26 13:03:52 +0530

    consloe

--- hidden/jso/note.js
+++ hidden/jso/note.js
@@ -19,38 +19,47 @@ function w5() {
 
 function w6() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 }
 
 function w7() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 }
 
 function w8() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 }
 
 function w9() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 
 }
 function w10() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 
 }
 function w11() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 
 }
 function w12() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 
 }
 function w13() {
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 
 }
 function model(){
     var url="https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka";
+    console.log("initiated")
 
 }
 function exam1(){
