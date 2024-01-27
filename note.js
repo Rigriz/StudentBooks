b6e6ef78e5459da4ab93eaa20054da6bb4721467
Roy <157291828+Rigriz@users.noreply.github.com>
2024-01-27 20:20:43 +0530

    link in note.js

--- note.js
+++ note.js
@@ -2,11 +2,10 @@
 function data(note){
     localStorage.setItem('data', note);
     console.log("initiated")
-    let url="./week"+note+".html";
+    let url="./hidden/fd/week"+note+".html";
     console.log(url)
     windows.location.href=url;
 }
-
 function exam1(){}
    /*  window.location.assign(
         "https://1drv.ms/w/s!AvY3abs-bIvAgTep_PmsgRRoCfii?e=FTwhqd"
