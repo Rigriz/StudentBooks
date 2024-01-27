9a49bc12272b500f61ed2d5827f9f8b84a21e5dd
Roy <157291828+Rigriz@users.noreply.github.com>
2024-01-27 19:58:00 +0530

    week update1

--- pd.js
+++ pd.js
@@ -9,46 +9,46 @@ console.log("entering obja")
     }
     if (aphint = 2) {
         var url ="./hidden/fd/Week2.html"
-        console.log("week1 set");
+        console.log("week2 set");
     }
     if (aphint = 3) {
         var url = "./hidden/fd/Week3.html"
-        console.log("week1 set");
+        console.log("week3 set");
     }
     if (aphint = 4) {
         var url = "https://www.google.com"
-        console.log("week1 set");
+        console.log("week4 set");
     }
     if (aphint = 5) {
         var url = "./hidden/fd/Week1.pdf"
-        console.log("week1 set");
+        console.log("week5 set");
     }
     if (aphint = 6) {
         var url = "./hidden/fd/Week1.pdf"
-        console.log("week1 set");
+        console.log("week6 set");
     }
     if (aphint = 7) {
         var url = "./hidden/fd/Week1.pdf"
-        console.log("week1 set");
+        console.log("week7 set");
     }
     if (aphint = 8) {
         var url = "./hidden/fd/Week1.pdf"
-        console.log("week1 set");
+        console.log("week8 set");
     }
     if (aphint = 9) {
         var url = "https://www.google.com"
-        console.log("week1 set");
+        console.log("week9 set");
     }
     if (aphint = 10) {
         var url = "https://www.google.com"
-        console.log("week1 set");
+        console.log("week10 set");
     }
     if (aphint = 12) {
         var url = "https://www.google.com"
-        console.log("week1 set");
+        console.log("week11 set");
     }
     if (aphint = 13) {
         var url = "https://www.google.com"
-        console.log("week1 set");
+        console.log("week12 set");
     }  
     console.log(url);
