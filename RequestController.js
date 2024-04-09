a2053d25e67503ddacbd40a6f76f0f2d0a21323a
sammith <isrpcollege@gmail.com>
2024-04-09 14:18:19 +0530

    Data Structure HTML UPDATE

--- RequestController.js
+++ RequestController.js
@@ -43,11 +43,33 @@ class dsap{
         var notes = note;
         console.log(notes)
       //  url="./hidden/fd/Week1.html"
-        location.assign("./hidden/dsa/Week"+notes+".html")
+        location.assign("./hidden/dsa/Week"+notes+".html");
     }
     labprograms(){
-         location.assign("./hidden/dsa/LAB_PROGRAMS.html")
+         location.assign("./hidden/dsa/LAB_PROGRAMS.html");
     }
+    Merge_Sort(){
+        location.assign("./hidden/dsa/Merge_Sort_Alg.html");
+    }
+    Quick_Sort(){
+        location.assign("./hidden/dsa/Quick_Sort.html");
+    }
+    SingleLinkList(){
+        location.assign("./hidden/dsa/Single_Linked_List.html");
+    }
+    Queue(){
+      location.assign("./hidden/dsa/Queue.html");    
+    }
+    Hash_Table(){
+         location.assign("./hidden/dsa/Hash_Table.html");  
+    }
+    Binery_Search(){
+        location.assign("./hidden/dsa/Bineary_Search.html");
+    }
+    Binery_Search_Tree(){
+        location.assign("./hidden/dsa/Bineary_Search_Tree.html");
+    }
+
 }
 const main = new mains();
 const osa = new os();
