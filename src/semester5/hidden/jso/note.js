baae26f3fb2aecaf1e9dc968fe914146d76f58ea
Roy <157291828+Rigriz@users.noreply.github.com>
2024-05-18 16:56:33 +0530

    semester 5 initiated

---
+++ src/semester5/hidden/jso/note.js
@@ -0,0 +1,82 @@
+function w1() {
+    window.location.assign("https://1drv.ms/w/s!AvY3abs-bIvAgSlw4D2iuzCoL5Ka");
+}
+function w2() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgSp3SCy73TvmKrE4?e=03FZZa"
+    );
+}
+function w3() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgS13JCWkfHYGZS_P?e=I54yK6"
+    );
+}
+function w4() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgSza7uWQp_rHAiAC?e=fXreOW"
+    );
+}
+function w5() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgS55_QgMFsXcvDg6?e=rga80B"
+    );
+}
+function w6() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgS_sUCb5PZ1GnadP?e=dQHbth"
+    );
+}
+function w7() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTNIsn3n1zqVZ2Iq?e=v7bGcT"
+    );
+}
+function w8() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTQn76JQCwELrd5e?e=pbusvL"
+    );
+}
+function w9() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTXBeCqcMhIYNYdD?e=miFqAC"
+    );
+}
+function w10() {
+    alert("Due to some issue we are unable to provide week 10 Notes");
+}
+function w11() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTaDO3Q_btcjktgb?e=aUpuli"
+    );
+}
+function w12() {
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTep_PmsgRRoCfii?e=FTwhqd"
+    );
+}
+function w13() {
+    alert("Due to some issue we are unable to provide week 13 Notes");
+}
+function model(){
+    window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTwxbdoyWqRJvV7e?e=J0Y8kt"
+    );
+}
+function exam1(){
+   /*  window.location.assign(
+        "https://1drv.ms/w/s!AvY3abs-bIvAgTep_PmsgRRoCfii?e=FTwhqd"
+    );
+    */
+   alert("Currently this page is not available");
+}
+function exam2(){
+    window.location.assign(
+        "https://1drv.ms/b/s!AvY3abs-bIvAgTuslvgQYCwFCJsu?e=g1prFF"
+    );
+}
+function exam3(){
+    window.location.assign(
+        "https://1drv.ms/b/s!AvY3abs-bIvAgTpNm0r7N2q7rfYm?e=SaHKsu"
+    );
+}
+
---
+++ src/semester5/links/s.js
@@ -0,0 +1,81 @@
+//html 
+ function html(){
+    document.getElementById("frame").src="https://www.javatpoint.com/html-tutorial";
+  }
+  function hary(){
+    window.location.href ="https://www.codewithharry.com/tutorial/html-home/";
+  }
+  function tot(){
+    window.location.href ="https://www.tutorialspoint.com/html/index.htm";
+  }
+  //javascript
+  function jav(){
+    document.getElementById("frame").src="https://www.javatpoint.com/javascript-tutorial";
+  }
+  function ja1(){
+    window.location.href ="https://www.codewithharry.com/tutorial/js/";
+  }
+  function j1(){
+    window.location.href ="https://www.tutorialspoint.com/javascript/index.htm";
+  }
+  //react
+  function re(){
+    document.getElementById("frame").src="https://www.javatpoint.com/reactjs-tutorial";
+  }
+  function rea(){
+    window.location.href ="https://www.codewithharry.com/tutorial/react-home/";
+  }
+  function re1(){
+    window.location.href  ="https://www.tutorialspoint.com/reactjs/index.htm";
+  }
+  //php
+  function php(){
+    document.getElementById("frame").src="https://www.javatpoint.com/php-tutorial";
+  }
+  function ph(){
+    window.location.href ="https://www.codewithharry.com/tutorial/php/";
+  }
+  function ph1(){
+    window.location.href ="https://www.tutorialspoint.com/php/index.htm";
+  }
+  //mongo
+  function mong(){
+    document.getElementById("frame").src="https://www.javatpoint.com/mongodb-tutorial";
+  }
+  function mon(){
+    window.location.href ="https://www.codewithharry.com/blogpost/mongodb-cheatsheet/";
+  }
+  function mo(){
+    window.location.href ="https://www.tutorialspoint.com/mongodb/index.htm";
+  }
+  
+  //docker
+  function docker(){
+    document.getElementById("frame").src="https://www.javatpoint.com/docker-tutorial";
+  }
+  function dock(){
+    window.location.href ="https://www.youtube.com/watch?v=WNUCAPKa44Y";
+  }
+  function do1()
+  {window.location.href ="https://www.tutorialspoint.com/docker/index.htm";
+  }
+    //git
+    function git(){
+      document.getElementById("frame").src="https://www.javatpoint.com/git";
+    }
+    function github(){
+      window.location.href ="https://www.codewithharry.com/videos/git-tut-beginners-hindi-1/";
+    }
+    function gi()
+    {
+      window.location.href ="https://www.tutorialspoint.com/git/index.htm";}
+       //ai
+    function ai(){
+      document.getElementById("frame").src="https://www.javatpoint.com/artificial-intelligence-ai";
+    }
+    function ia(){
+      window.location.href ="https://www.tutorialspoint.com/artificial_intelligence/index.htm";
+    }
+    function aii()
+    {
+      window.location.href ="https://www.w3schools.com/ai/ai_whatis.asp";}
